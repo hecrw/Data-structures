961d81f6a710360acbed15bf54c613a9036def10
hecrw <136959713+hecrw@users.noreply.github.com>
2023-08-28 15:33:21 +0300

    updated siftup and siftdown and added the public funcitons of a pq

--- prioriy_queue.cpp
+++ prioriy_queue.cpp
@@ -12,19 +12,59 @@ private:
     int parent(int i) {
         return (i - 1) / 2;
     }
-    int leftchild(int i) {
+    int leftChild(int i) {
         return 2 * i + 1;
     }
-    int rightchild(int i) {
+    int rightChild(int i) {
         return 2 * i + 2;
     }
 
-    void siftup(int i) {
-        while (i > 0 && heap[i] < heap[parent(i)]) {
-            std::swap(heap[i], heap[parent(i)]);
-        }
-    }
+	void siftup(int i) {
+		while (i != 0 && heap[i] > heap[parent(i)]) {
+			std::swap(heap[i], heap[parent(i)]);
+			i = parent(i);
+		}
+	}
     void siftdown(int i) {
-        int minindex = i,left = leftchild(i), right = rightchild(i);
+        		int largest = i;
+		if (leftChild(i) < heap.size() && heap[leftChild(i)] > heap[largest]) {
+			largest = leftChild(i);
+		}
+
+		if (rightChild(i) < heap.size() && heap[rightChild(i)] > heap[largest]) {
+			largest = rightChild(i);
+		}
+
+		if (largest != i) {
+			swap(heap[i], heap[largest]);
+			siftdown(largest);
+		}
     }
+public:
+	size_t size() {
+		return heap.size();
+	}
+	bool empty() {
+		return heap.size() == 0;
+	}
+	T top() {
+		if (empty()) {
+			throw runtime_error("queue is empty");
+		}
+		return heap[0];
+	}
+	void push(T value) {
+		heap.push_back(value);
+		siftup(int(heap.size() - 1));
+	}
+	T extractMax() {
+		if (empty()) {
+			throw runtime_error("queue is empty");
+		}
+		T temp = heap[0];
+		swap(heap[0], heap[heap.size() - 1]);
+		heap.pop_back();
+		siftdown(0);
+		return temp;
+	}
 };
